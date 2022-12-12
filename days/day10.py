64043c2c0a24ecd44917b5c6cd34e14a8ded2a83
gavin <gavin.har@pm.me>
2022-12-11 20:28:50 -0500

    Add day 10

---
+++ days/day10.py
@@ -0,0 +1,22 @@
+from functools import reduce
+
+# f = open('../input/day10.txt')
+f = open('../input/day10.txt')
+
+# calculate value of x during each cycle
+x = 1
+xs = []
+for line in f:
+    xs.append(x)
+    if line.strip() != 'noop':
+        xs.append(x)
+        x += int(line.strip().split(' ')[1])
+
+# part 1
+indexes = [20, 60, 100, 140, 180, 220]
+print(reduce(lambda a, c: a + xs[c - 1] * c, indexes, 0))
+
+# part 2
+pixels = list(map(lambda idx: '#' if xs[idx] - 1 <= (idx % 40) <= xs[idx] + 1 else '.', range(240)))
+for i in range(0, 240, 40):
+    print(''.join(pixels[i:i+40]))
