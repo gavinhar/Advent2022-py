96138141095ead0a108c93f309b1d1b1123768bb
gavin <gavin.har@pm.me>
2022-12-22 18:36:58 -0500

    Add day 12 and 13

---
+++ days/day12.py
@@ -0,0 +1,72 @@
+from typing import IO, Tuple, List, Optional
+
+
+def read_input(input: IO, part2: bool) -> Tuple[List[List[int]], List[Tuple[int, int]], Tuple[int, int]]:
+    grid = []
+    start, end = [], None
+    row, col = 0, 0
+    for line in input:
+        grid.append([])
+        col = 0
+        for c in line.strip():
+            if c == 'S' or (part2 and c == 'a'):
+                grid[row].append(0)
+                start.append((row + 1, col + 1))
+            elif c == 'E':
+                grid[row].append(25)
+                end = (row + 1, col + 1)
+            else:
+                grid[row].append(int(c, 36) - int('a', 36))
+            col += 1
+        row += 1
+
+    # pad the grid to make search logic simpler
+    dim = (len(grid), len(grid[0]))
+    for i in range(dim[0]):
+        grid[i].insert(0, 100)
+        grid[i].append(100)
+    grid.insert(0, [100] * (2 + dim[1]))
+    grid.append([100] * (2 + dim[1]))
+
+    return grid, start, end
+
+
+def solution(input: IO, part2: bool):
+    grid, start, end = read_input(input, part2)
+
+    visited = set()
+    todo = list(map(lambda s: (s, 0), start))
+
+    while True:
+        curr, steps = todo.pop(0)
+
+        if curr == end:
+            return steps
+
+        if curr in visited:
+            continue
+
+        visited.add(curr)
+        x, y = curr
+        curr_alt = grid[x][y]
+        if grid[x - 1][y] <= curr_alt + 1:
+            todo.append(((x - 1, y), steps + 1))
+        if grid[x + 1][y] <= curr_alt + 1:
+            todo.append(((x + 1, y), steps + 1))
+        if grid[x][y - 1] <= curr_alt + 1:
+            todo.append(((x, y - 1), steps + 1))
+        if grid[x][y + 1] <= curr_alt + 1:
+            todo.append(((x, y + 1), steps + 1))
+
+
+with open('../input/day12-sample.txt') as f:
+    print(solution(f, False))
+
+with open('../input/day12-sample.txt') as f:
+    print(solution(f, True))
+
+with open('../input/day12.txt') as f:
+    print(solution(f, False))
+
+with open('../input/day12.txt') as f:
+    print(solution(f, True))
---
+++ days/day13.py
@@ -0,0 +1,97 @@
+import functools
+from typing import IO, Optional, List, Union
+
+def parse(l: str) -> List:
+    line = l[1:len(l)-1]
+    curr = []
+    rv = curr
+    prev = []
+    num = ''
+
+    for c in line:
+        if c == '[':
+            prev.append(curr)
+            curr = []
+            prev[-1].append(curr)
+        elif c == ']':
+            if len(num) > 0:
+                curr.append(int(num))
+                num = ''
+            curr = prev.pop()
+        elif c == ',':
+            if len(num) > 0:
+                curr.append(int(num))
+                num = ''
+        else:
+            num = num + c
+    if len(num) > 0:
+        curr.append(int(num))
+    return rv
+
+
+def is_list(v):
+    return isinstance(v, list)
+
+
+def is_sorted(a, b):
+    if is_list(a) and not is_list(b):
+        b = [b]
+    elif is_list(b) and not is_list(a):
+        a = [a]
+    elif not is_list(a) and not is_list(b):
+        if a == b:
+            return None
+        return a < b
+
+    for i in range(min(len(a), len(b))):
+        v_sorted = is_sorted(a[i], b[i])
+        if v_sorted is not None:
+            return v_sorted
+
+    if len(a) == len(b):
+        return None
+    return len(a) < len(b)
+
+
+def sort(packets):
+    while True:
+        ordered = True
+        for i in range(1, len(packets)):
+            if not is_sorted(packets[i-1], packets[i]):
+                ordered = False
+                packets[i-1], packets[i] = packets[i], packets[i-1]
+        if ordered:
+            return
+
+
+def solution(f: IO) -> int:
+    total = 0
+    idx = 1
+    packets = [[[2]], [[6]]]
+
+    while True:
+        a = parse(f.readline().strip())
+        b = parse(f.readline().strip())
+        packets.extend([a, b])
+
+        if is_sorted(a, b):
+            total += idx
+        idx += 1
+
+        if f.readline() == '':
+            sort(packets)
+            return total, (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
+
+
+with open('../input/day13-sample.txt') as f:
+    part1, part2 = solution(f)
+    assert part1 == 13
+    print(part1)
+    print(part2)
+    print()
+
+
+with open('../input/day13.txt') as f:
+    part1, part2 = solution(f)
+    print(part1)
+    print(part2)
